d1f48a0983e35cc702702bcc38c10b969fb76c01
jamesigibbs <jamesigibbs180@gmail.com>
2020-07-19 20:05:37 +0000

    Added main pages and started activites page

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -3,15 +3,14 @@ let winWidth = $(window).width();
 
 $(window).resize(function(){
     winWidth = $(window).width();
-});
 
-if (winWidth < 990) {
-   $(".nav-list").append(`<li class="nav-item">
-									<a class="nav-link" href="#">Shopping Cart</a>
-								</li>`)
+    if (winWidth < 990) {
+   $(".nav-list").append(`<li class="nav-item"><a class="nav-link" href="#">Shopping Cart</a></li>`)
 }
 else {
-   $(".nav-list").remove(`<li class="nav-item">
-									<a class="nav-link" href="#">Shopping Cart</a>
-								</li>`)
+    let html = $(".nav-list").html();
+    html = html.replace(`<li class="nav-item"><a class="nav-link" href="#">Shopping Cart</a></li>`, "");
+    $(".nav-list").html(html);
 }
+});
+
