ea9c5839b00733865dea74e23bfe4b9e56f17197
jamesigibbs <jamesigibbs180@gmail.com>
2020-07-27 23:48:25 +0000

    Found location of colour in slot class

--- assets/scripts/book.js
+++ assets/scripts/book.js
@@ -2,13 +2,14 @@
 //Gerate Popup
 
 $(".slot").click(function () {
-	$(this).css("background-color", "#FD6A02");
+    checkColour()
+    $(this).css("background-color", "#FD6A02");
 	$("#myModal").modal("show");
 	let cSlot = this;
 
 	$("#addCart").click(function () {
 		$(cSlot).css("background-color", "#ff0000");
-		$("#myModal").modal("hide");
+        $("#myModal").modal("hide");
 	});
 
 	$("#cancel").click(function () {
@@ -17,15 +18,17 @@ $(".slot").click(function () {
 
 	$("button.close").click(function () {
 		$(cSlot).css("background-color", "#008000");
-	});
+    });
 });
 
-let colourCheck = document.getElementsByClassName("slot");
-for(let i = 0; i < colourCheck.length; i++) {
-    if(colourCheck[i].style.backgroundColor == "#ff0000") {
-        $("#myModal").modal("hide")
+function checkColour() {
+ let colourCheck = document.getElementsByClassName("slot")
+    for(let i = 0; i < colourCheck.length; i++) {
+        let colour = getComputedStyle(colourCheck[i]).backgroundColor
+        console.log(colour);
     }
 }
+
 
 // if (document.getElementsByClassName("slot").style.background-color == "#ff0000") {
 //     $(this).click(function () {
