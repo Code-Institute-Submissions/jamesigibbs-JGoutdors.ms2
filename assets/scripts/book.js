98d0145b5a05072bafae23253402049a7dc53ace
jamesigibbs <jamesigibbs180@gmail.com>
2020-07-30 23:42:04 +0000

    started shopping cart and added styling to modal

--- assets/scripts/book.js
+++ assets/scripts/book.js
@@ -4,7 +4,6 @@
 
 $(".slot").click(function () {
      cSlot = this;
-     console.log(cSlot);
     if (cSlot.style.backgroundColor == "rgb(0, 128, 0)") {
         $(cSlot).css("background-color", "#FD6A02");
 	    $("#myModal").modal("show");
@@ -13,6 +12,10 @@ $(".slot").click(function () {
 	$("#addCart").click(function () {
 		$(cSlot).css("background-color", "rgb(255, 0, 0)");
         $("#myModal").modal("hide");
+        activityObj.Activity = activity;
+        activityObj.Price = price;
+        cart.push(activityObj)
+        console.log(cart)
 	});
 
 	$("#cancel").click(function () {
@@ -24,4 +27,19 @@ $(".slot").click(function () {
     });
 });
 
+//Labels
+let activity;
+let price;
+let activityObj = new Object();
+
+$(".dropdown-item").click(function () {
+   activity = $("span:first", this).html();
+   price =  $("span:last", this).html();
+    $("#activity").html(activity);
+    $("#price").html(price);
+});
+
 //Shopping Cart
+let cart = [
+    
+]
